8c1550b961cd6510b53a168b8485dffead496bb1
mykhailo.zheliaskov <mykhailo.zheliaskov@decerto.pl>
2018-07-09 08:47:21 +0200

    Cron test

--- db/index.js
+++ db/index.js
@@ -10,6 +10,7 @@ let db = {};
 
 let sequelize = new Sequelize(config.database, config.username, config.password, {
 	dialect: "sqlite",
+	dialectOptions: { useUTC: false, },
 	storage: path.join(__dirname, 'currency-rates.db'),
 	define: {
 		underscored: true,
